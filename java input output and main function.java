ec89e1fda453d88f677b8ef0ffda3a60e980e1e0
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2019-03-15 12:39:13 +0600

    Create java input output and main function.java

---
+++ java input output and main function.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int[] b;
+	
+	static void func(String substr) {
+		int m = substr.length();
+		b = new int[m + 12];
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		String str;
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		int ts = Integer.parseInt(sc.readLine());
+		System.out.println("Test case #" + t);
+		str = sc.readLine();
+		System.out.println(i + " " + (i / temp));
+	}
+}
