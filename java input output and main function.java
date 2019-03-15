1f7f4258905846013fc9012ebecf000b9a8c65fd
Sourav Chakraborty <sourovchakraborty420@gmail.com>
2019-03-15 12:51:37 +0600

    Update java input output and main function.java

--- java input output and main function.java
+++ java input output and main function.java
@@ -1,21 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 	static int[] b;
-	
+
 	static void func(String substr) {
 		int m = substr.length();
 		b = new int[m + 12];
 	}
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+
+	public static void main(String[] args) {
 		String str;
-		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
-		int ts = Integer.parseInt(sc.readLine());
+		Scanner sc = new Scanner(System.in);
+		int ts = sc.nextInt();
 		System.out.println("Test case #" + t);
-		str = sc.readLine();
+		str = sc.nextLine();
+		func(str);
 		System.out.println(i + " " + (i / temp));
 	}
 }
